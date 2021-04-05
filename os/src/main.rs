fd299cb751b7a87354f931f9c39e2e61688420c0
miquanyiu <1135477643@qq.com>
2021-04-05 16:41:55 +0800

    fix: page table

--- os/src/main.rs
+++ os/src/main.rs
@@ -3,6 +3,12 @@
 #![feature(global_asm)]
 #![feature(llvm_asm)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
+
+#[macro_use]
+extern crate bitflags;
 
 #[macro_use]
 mod console;
@@ -14,6 +20,7 @@ mod loader;
 mod config;
 mod task;
 mod timer;
+mod mm;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -32,8 +39,11 @@ fn clear_bss() {
 pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
+    mm::init();
+    println!("[kernel] back to world!");
+    mm::remap_test();
     trap::init();
-    loader::load_apps();
+    //trap::enable_interrupt();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
